8fb785141fb6db94c9aa9ee2b9f27f131588139d
Matúš Tomlein <matus.tomlein@gmail.com>
2022-05-23 11:52:16 +0200

    Fix closing threads in AsyncEmitter when exitting app (close #46)

--- SnowplowTracker/SnowplowTracker/Emitters/AsyncEmitter.cs
+++ SnowplowTracker/SnowplowTracker/Emitters/AsyncEmitter.cs
@@ -146,7 +146,7 @@ namespace SnowplowTracker.Emitters
 			while (sending) {
 				// Wait for something to be sent!
 				lock (emitLock) {
-					while (eventStore.GetEventCount() == 0) {
+					while (sending && eventStore.GetEventCount() == 0) {
 						Monitor.Wait(emitLock);
 					}
 				}
